160857cc32d9ffbff7916cae9c22f28c7220fd37
MaishaTahsin <rubaitreshad@gmail.com>
2025-06-21 11:55:19 +0600

    Implement Marquee & Design Navbar

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
+// import Navbar from "daisyui/components/navbar";
 
 const HomeLayout = () => {
   return (
     <div>
       <header>
         <Header></Header>
+        <section className="w-11/12 mx-auto my-3">
+          <LatestNews></LatestNews>
+        </section>
+
+
+        <nav className="w-11/12 mx-auto my-3">
+          <Navbar></Navbar>
+        </nav>
+
+
       </header>
       <main>
         <section className="left_nav"></section>
