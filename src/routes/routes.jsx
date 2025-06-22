920c1985d4ba031e63219491a6e52d323fa52f9b
MaishaTahsin <rubaitreshad@gmail.com>
2025-06-23 02:56:04 +0600

    Load & Display News Details Page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -5,6 +5,7 @@ import CategoryNews from "../pages/CategoryNews";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import AuthLayout from "../layouts/AuthLayout";
+import NewsDetails from "../pages/NewsDetails";
 
 const router = createBrowserRouter([
   {
@@ -37,8 +38,9 @@ const router = createBrowserRouter([
     ],
   },
   {
-    path: "/news",
-    element: <h2>News Layout</h2>,
+    path: "/news-details/:id",
+    element: <NewsDetails></NewsDetails>,
+    loader: () => fetch("/data/news.json")
   },
   {
     path: "/*",
