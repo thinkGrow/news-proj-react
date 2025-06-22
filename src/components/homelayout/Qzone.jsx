6ab317fb6e7526ee5615952fb166a875340e1682
MaishaTahsin <rubaitreshad@gmail.com>
2025-06-22 07:29:46 +0600

    use navigate to load default home page

--- src/components/homelayout/Qzone.jsx
+++ src/components/homelayout/Qzone.jsx
@@ -16,3 +16,4 @@ const QZone = () => {
 };
 
 export default QZone;
+ 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  return <Navigate to="/category/1">
+
+  </Navigate>
 };
 
 export default Home;
